e3b20309ec1ca9dc8d3069b0d192d7becfd80842
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-08-18 00:21:44 +0530

    added Cart & Modal Component

--- FoodOrder-project/src/App.js
+++ FoodOrder-project/src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Carts/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
---
+++ FoodOrder-project/src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import styles from "./Modal.module.css";
+import ReactDom from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+export default Modal;
