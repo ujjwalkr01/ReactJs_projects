297416c2e4eae017962891e53c7c87068f81944b
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-08-20 03:33:23 +0530

    update default value of Amount

--- FoodOrder-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ FoodOrder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -36,7 +36,7 @@ const MealItemForm = (props) => {
           min: "1",
           max: "5",
           step: "1",
-          defaultValue: "1",
+          defaultValue: "0",
         }}
       />
       <button>+ Add</button>
